21a857a61088434cfc053b8c043fcdbd894dce8e
Ankush Kumar <ankushkashyap0000@gmail.com>
2021-06-12 17:26:08 +0530

    added feature of update and delete address

--- src/address/api/mutation.js
+++ src/address/api/mutation.js
@@ -1,11 +1,25 @@
-const { addAddressLogic } = require("../logic/logic");
+const {
+  addAddressLogic,
+  updateAddressLogic,
+  deleteAddressLogic,
+} = require("../logic/logic");
 
 const addAddress = async (parent, args, context, info) => {
   return addAddressLogic(parent, args, context, info);
 };
 
+const updateAddress = async (parent, args, context, info) => {
+  return updateAddressLogic(parent, args, context, info);
+};
+
+const deleteAddress = async (parent, args, context, info) => {
+  return deleteAddressLogic(parent, args, context, info);
+};
+
 const AddressMutation = {
   addAddress: addAddress,
+  updateAddress: updateAddress,
+  deleteAddress: deleteAddress,
 };
 
 module.exports = { AddressMutation };
--- src/address/logic/logic.js
+++ src/address/logic/logic.js
@@ -1,4 +1,9 @@
-const { getAllAddressData, addAddressData } = require("../data/data");
+const {
+  getAllAddressData,
+  addAddressData,
+  updateAddressData,
+  deleteAddressData,
+} = require("../data/data");
 
 const getAllAddressLogic = () => {
   return getAllAddressData();
@@ -18,4 +23,30 @@ const addAddressLogic = (parent, args, context, info) => {
   return addAddressData(newAddress);
 };
 
-module.exports = { getAllAddressLogic, addAddressLogic };
+const updateAddressLogic = (parent, args, context, info) => {
+  const addressID = args.addressID;
+  // console.log(addressID);
+  const updatedAddress = {
+    fullName: args.fullName,
+    phoneNumber: args.phoneNumber,
+    pincode: args.pincode,
+    state: args.state,
+    city: args.city,
+    HouseNo: args.HouseNo,
+    area: args.area,
+    landmark: args.landmark,
+  };
+  return updateAddressData(addressID, updatedAddress);
+};
+
+const deleteAddressLogic = async (parent, args, context, info) => {
+  const addressID = args.addressID;
+  return deleteAddressData(addressID);
+};
+
+module.exports = {
+  getAllAddressLogic,
+  addAddressLogic,
+  updateAddressLogic,
+  deleteAddressLogic,
+};
