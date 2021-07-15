eed67ed66fbd3d61b7063458f08eea990ef72ab3
Ankush Kumar <ankushkashyap0000@gmail.com>
2021-07-15 17:49:11 +0530

    added getSingleOrder Query

--- src/order/api/query.js
+++ src/order/api/query.js
@@ -1,11 +1,16 @@
-const { getAllOrdersLogic } = require("../logic/logic");
+const { getAllOrdersLogic, getOrderByIdLogic } = require("../logic/logic");
 
 const getAllOrders = () => {
   return getAllOrdersLogic();
 };
 
+const getOrderById = (parent, args, context, info) => {
+  return getOrderByIdLogic(parent, args, context, info);
+};
+
 const OrderQuery = {
   getAllOrders: getAllOrders,
+  getOrderById: getOrderById,
 };
 
 module.exports = { OrderQuery };
